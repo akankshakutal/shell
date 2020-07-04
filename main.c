5386e63cf7a8fe358bedf029dfe79fc92bcf96a1
akankshakutal <akankshk@thoughtworks.com>
2020-07-04 14:24:33 +0530

    Run multiple commands

--- main.c
+++ main.c
@@ -1,27 +1,34 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-    printf("myShell $ ");
-
-    char input[256] = {0x0};
-    char *ptr = input;
-    char *args[64] = {NULL};
-
-    fgets(input, 255, stdin);
-
-    for (int i = 0; i < sizeof(args) && *ptr; ptr++)
+    while (1)
     {
-        if (*ptr == ' ')
-            continue;
-        if (*ptr == '\n')
-            break;
-        for (args[i++] = ptr; *ptr && *ptr != ' ' && *ptr != '\n'; ptr++)
-            ;
-        *ptr = '\0';
-    }
+        printf("myShell $ ");
 
-    execvp(args[0], args);
+        char input[256] = {0x0};
+        char *ptr = input;
+        char *args[64] = {NULL};
+
+        fgets(input, 255, stdin);
+
+        for (int i = 0; i < sizeof(args) && *ptr; ptr++)
+        {
+            if (*ptr == ' ')
+                continue;
+            if (*ptr == '\n')
+                break;
+            for (args[i++] = ptr; *ptr && *ptr != ' ' && *ptr != '\n'; ptr++)
+                ;
+            *ptr = '\0';
+        }
+
+        if (fork() == 0)
+            exit(execvp(args[0], args));
+        wait(NULL);
+    }
 }
